6d18b7dede349593987719a670d11edf39b55788
Carel155 <carel_martten@hotmail.com>
2017-10-27 14:22:32 +0300

    add server to port 80

--- app/app.js
+++ app/app.js
@@ -1,4 +1,14 @@
-var builder = require('botbuilder');
+var builder = require('botbuilder'),
+    restify = require('resitfy');
+
+const server = restify.createServer({
+  name: 'Bot Toivo',
+  version: '1.0.0'
+})
+
+server.listen(80, function(){
+  console.log("Toivo online at port 80");
+});
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector, function (session) {

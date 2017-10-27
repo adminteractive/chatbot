9e76af57156495dfcb6b0c7b7da5466b1d2812db
Carel155 <carel_martten@hotmail.com>
2017-10-27 15:49:48 +0300

    Proof of conecept

--- app/app.js
+++ app/app.js
@@ -1,16 +1,27 @@
 var builder = require('botbuilder'),
-    restify = require('resitfy');
+    restify = require('restify'),
+    teams = require('botbuilder-teams');
 
 const server = restify.createServer({
   name: 'Bot Toivo',
   version: '1.0.0'
 })
 
-server.listen(80, function(){
-  console.log("Toivo online at port 80");
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.listen(3000, function(){
+  console.log("Toivo onlineasd at port 80");
 });
 
-var connector = new builder.ConsoleConnector().listen();
+var connector = new teams.TeamsChatConnector({
+    appId: '',
+    appPassword: ''
+});
+
+server.post('/api/messages', connector.listen());
+
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+  session.send("Test");
 });

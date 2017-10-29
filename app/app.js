bd01d0ced485ee924ac3be4adbe5a4bd57658789
calech <calech@ttu.ee>
2017-10-30 00:17:06 +0200

    implement mongodb

--- app/app.js
+++ app/app.js
@@ -1,11 +1,28 @@
 var builder = require('botbuilder'),
     restify = require('restify'),
-    teams = require('botbuilder-teams');
+    teams = require('botbuilder-teams'),
+    mongoose = require('mongoose'),
+    dotenv = require('dotenv').config();
 
 const server = restify.createServer({
   name: 'Bot Toivo',
   version: '1.0.0'
-})
+});
+
+const mongoOptions = {
+  user: process.env.MONGODB_USER,
+  pass: process.env.MONGODB_PASSWORD
+}
+
+mongoose.connect(process.env.MONGODB_URL, mongoOptions);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log("Connected to databas!");
+});
 
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
